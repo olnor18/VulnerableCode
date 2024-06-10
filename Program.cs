fbd9cabdb24a4c70c508a2925d2bc81e5b701118
Oliver Nordestgaard <42771071+olnor18@users.noreply.github.com>
2024-06-10 13:35:23 +0000

    Ready for production

---
+++ Program.cs
@@ -0,0 +1,41 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/weatherforecast", (string city) =>
+{
+    // Start the child process.
+    Process p = new Process();
+    // Redirect the output stream of the child process.
+    p.StartInfo.UseShellExecute = false;
+    p.StartInfo.RedirectStandardOutput = true;
+    p.StartInfo.FileName = "curl.exe";
+    p.StartInfo.Arguments = "-H 'X-Api-Key: YOUR_API_KEY' https://api.api-ninjas.com/v1/weather?city="+city
+    p.Start();
+    // Do not wait for the child process to exit before
+    // reading to the end of its redirected stream.
+    // p.WaitForExit();
+    // Read the output stream first and then wait.
+    string output = p.StandardOutput.ReadToEnd();
+    p.WaitForExit();
+    return output;
+})
+.WithName("GetWeatherForecast")
+.With
+.WithOpenApi();
+
+app.Run();
